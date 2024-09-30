45bc79f45d611a12f03889e6c96a62e893d6a351
ajayrana78767 <148841039+ajayrana78767@users.noreply.github.com>
2024-09-30 15:53:04 +0530

    Refactor LoginScreen
    "Removed appBar, added welcome text, adjusted spacing and styling, and added sign-up navigation and social media login buttons"

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:personal_finance_tracker_app/screens/home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,21 +6,23 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text(
+              'Welcome Back!',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
             const TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20),
             const TextField(
               obscureText: true,
               decoration: InputDecoration(
@@ -29,15 +30,31 @@ class LoginScreen extends StatelessWidget {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  HomeScreen()),
-                );
+                // Handle login
               },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+              ),
               child: const Text('Login'),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                // Handle sign-up navigation
+              },
+              child: const Text('Don\'t have an account? Sign up'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(icon: const Icon(Icons.facebook), onPressed: () {}),
+                IconButton(
+                    icon: const Icon(Icons.g_translate), onPressed: () {}),
+                IconButton(icon: const Icon(Icons.apple), onPressed: () {}),
+              ],
             ),
           ],
         ),
