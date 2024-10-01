4e278ca940fc0100b5a4cdab3762cc6e5a24b619
ajayrana78767 <148841039+ajayrana78767@users.noreply.github.com>
2024-10-01 15:42:10 +0530

    "Refactored AddExpenseScreen with updated UI styles, colors, and layouts."

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -18,119 +18,163 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Color primaryColor = Colors.blue; // Define a primary color
+    final Color backgroundColor =
+        Colors.blue[50]!; // Lighter shade for the background
+
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Add Expense',style: TextStyle(color: Colors.white),),
+        title: const Text('Add Expense'),
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,color: Colors.white,),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        backgroundColor: Colors.blue,
+        backgroundColor: primaryColor, // Set app bar color
       ),
-      body: SingleChildScrollView(
-        // Wrap the body in a SingleChildScrollView
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Amount Field
-                TextFormField(
-                  controller: _amountController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Amount',
-                    border: OutlineInputBorder(),
+      body: Container(
+        color: backgroundColor, // Set body background color
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Amount Field with a unique style
+                  TextFormField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Amount',
+                      prefixIcon: const Icon(Icons.attach_money),
+                      labelStyle: const TextStyle(color: Colors.green),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Colors.green),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide:
+                            const BorderSide(color: Colors.green, width: 2),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an amount';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter an amount';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                // Category Dropdown
-                DropdownButtonFormField<String>(
-                  decoration: const InputDecoration(
-                    labelText: 'Category',
-                    border: OutlineInputBorder(),
+                  // Category Dropdown with a custom border
+                  DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: 'Category',
+                      prefixIcon: const Icon(Icons.category),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    value: _selectedCategory,
+                    items: _categories.map((String category) {
+                      return DropdownMenuItem<String>(
+                        value: category,
+                        child: Text(category),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedCategory = value;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select a category';
+                      }
+                      return null;
+                    },
                   ),
-                  value: _selectedCategory,
-                  items: _categories.map((String category) {
-                    return DropdownMenuItem<String>(
-                      value: category,
-                      child: Text(category),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      _selectedCategory = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please select a category';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                // Description Field (optional)
-                TextFormField(
-                  controller: _descriptionController,
-                  maxLines: 3,
-                  decoration: const InputDecoration(
-                    labelText: 'Description (optional)',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Date Picker
-                TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: _selectedDate == null
-                        ? 'Select Date'
-                        : 'Date: ${DateFormat('yyyy-MM-dd').format(_selectedDate!)}',
-                    border: const OutlineInputBorder(),
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.calendar_today),
-                      onPressed: _pickDate,
+                  // Description Field (optional) with unique text field style
+                  TextFormField(
+                    controller: _descriptionController,
+                    maxLines: 3,
+                    decoration: InputDecoration(
+                      labelText: 'Description (optional)',
+                      prefixIcon: const Icon(Icons.description),
+                      labelStyle: const TextStyle(color: Colors.purple),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Colors.purple),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide:
+                            const BorderSide(color: Colors.purple, width: 2),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                // Receipt Upload Button
-                ElevatedButton.icon(
-                  onPressed: _uploadReceipt,
-                  icon: const Icon(Icons.attach_file),
-                  label: const Text('Upload Receipt'),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
+                  // Date Picker with a unique field style
+                  TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: _selectedDate == null
+                          ? 'Select Date'
+                          : 'Date: ${DateFormat('yyyy-MM-dd').format(_selectedDate!)}',
+                      prefixIcon: const Icon(Icons.calendar_today),
+                      labelStyle: const TextStyle(color: Colors.orange),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: const BorderSide(color: Colors.orange),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide:
+                            const BorderSide(color: Colors.orange, width: 2),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.date_range),
+                        onPressed: _pickDate,
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(height: 20),
 
-                const SizedBox(height: 20), // Add spacing at the bottom
-
-                // Save Button
-                ElevatedButton(
-                  onPressed: _saveExpense,
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
+                  // Receipt Upload Button with an icon
+                  ElevatedButton.icon(
+                    onPressed: _uploadReceipt,
+                    icon: const Icon(Icons.camera_alt),
+                    label: const Text('Upload Receipt'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
                   ),
-                  child: const Text('Save Expense'),
-                ),
-              ],
+
+                  const SizedBox(height: 20),
+
+                  // Save Button with full width
+                  ElevatedButton(
+                    onPressed: _saveExpense,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text('Save Expense'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
